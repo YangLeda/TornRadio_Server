04f55bfc69e12c23a8c41f8ef57dc745de19febf
杨乐达 <yangleda@outlook.com>
2023-01-24 21:13:40 +0800

    Update server.js

--- TornRadioServer/server.js
+++ TornRadioServer/server.js
@@ -70,7 +70,7 @@ async function fetchSpyDoc() {
   await sheet.loadCells();
   for (let i = 0; i < MAX_ROW_COUNT; i++) {  // each row
     let cell = sheet.getCell(i, 0);
-    if (cell.value.indexOf("[") > 0 && cell.value.indexOf("]") > 0) {
+    if (cell.value !== null && cell.value.indexOf("[") > 0 && cell.value.indexOf("]") > 0) {
       let id = cell.value.substring(cell.value.indexOf("["), cell.value.indexOf("]") + 1);
       console.log(id);
       let obj = new Object();
