df16f32f014d60a30e44707f383a770aa8fa2b77
杨乐达 <yangleda@outlook.com>
2023-01-24 19:35:17 +0800

    google sheets access

--- TornRadioServer/server.js
+++ TornRadioServer/server.js
@@ -2,34 +2,15 @@ import fetch from "node-fetch";
 import express from "express";
 import cors from "cors";
 import logger from "./logger.js";
-import GSheetReader from "g-sheets-api";
+import {GoogleSpreadsheet} from "google-spreadsheet";
 
 const app = express();
 const port = 3001;
 const SCHEDULE_INTERVAL = 600000;  // Fetch member details every 10 minutes
 const FACTION_ID_1 = "13737";
 const TORN_API_KEY = "";
-
-const options = {
-  apiKey: "",
-  sheetId: "",
-  sheetNumber: 1,
-  returnAllResults: false,
-}
-GSheetReader(options, results => {
-  logger("GSheetReader " + JSON.stringify(results));
-}).catch(err => {
-  logger("GSheetReader error: " + err);
-});
-
-let playerCache = new Map();
-
-logger("start");
-
-fetchAllFactionMembersToCache(FACTION_ID_1);
-setInterval(async () => {
-  fetchAllFactionMembersToCache(FACTION_ID_1);
-}, SCHEDULE_INTERVAL);
+const GOOGLE_API_KEY = "";
+const DOC_ID = "";
 
 // CORS
 app.use(
@@ -37,6 +18,11 @@ app.use(
     origin: "*",
   })
 );
+
+const doc = new GoogleSpreadsheet(GOOGLE_API_KEY);
+doc.useApiKey(DOC_ID);
+await doc.loadInfo(); // loads document properties and worksheets
+console.log(doc.title);
 
 // faction API
 app.get("/faction", async (req, res) => {
