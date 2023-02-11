b0a121c22ff53f4c758f40e81b5d376876ad93fd
杨乐达 <yangleda@outlook.com>
2023-02-12 05:58:03 +0800

    Update server.js

--- TornRadioServer/server.js
+++ TornRadioServer/server.js
@@ -112,7 +112,7 @@ async function fetchSpyDoc() {
     if (typeof (cellParsedLine.value) == "string" && cellParsedLine.value != "" && typeof (cellId.value) === "string" && cellId.value.indexOf("[") > 0 && cellId.value.indexOf("]") > 0 && !isNaN(cellId.value.substring(cellId.value.indexOf("[") + 1, cellId.value.indexOf("]")))) {
       let line = cellParsedLine.value.replace(/,/g, "");
       logger("Parse line: " + line);
-      let matches = line.match(/\d+/g);
+      let matches = line.match(/ \d+ /g);
       if (matches.length >= 4 && matches.length <= 5) {
         for (let j = 0; j < matches.length; j++) {
           sheet.getCell(i, j + 2).value = matches[j];
