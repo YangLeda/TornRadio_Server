034cb2a8ef08afc5a6009efc508c58137e40d070
杨乐达 <yangleda@outlook.com>
2023-02-24 16:34:29 +0800

    Update server.js

--- TornRadioServer/server.js
+++ TornRadioServer/server.js
@@ -111,9 +111,13 @@ async function fetchSpyDoc() {
   // Parse single lines and fill cells.
   singleLineRowIndexes.forEach((rowIndex) => {
     let line = sheet.getCell(rowIndex, 9).value;
-    let matches = line.match(/Strength: (\d+|N\/A) /g);
+    let matches = line.match(/Strength: \d+ /g);
     if (matches.length == 1) {
       sheet.getCell(rowIndex, 2).value = parseInt(matches[0].substring(10, matches[0].length - 1));
+    }
+    matches = line.match(/Strength: N\/A /g);
+    if (matches.length == 1) {
+      sheet.getCell(rowIndex, 2).value = "N/A";
     }
     
   });
